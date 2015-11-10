3da9197315cfb8255f3a0f1b0eb84cae2028499f
aestrivex <aestrivex@gmail.com>
2015-11-10 10:42:47 -0500

    fix merge conflict in load_samples in py3 branch

--- test/load_samples.py
+++ test/load_samples.py
@@ -4,17 +4,6 @@ import os
 
 MAT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mats')
 mat_path = lambda fname: os.path.join(MAT_DIR, fname)
-<<<<<<< HEAD
-
-def load_sample(thres=1):
-	return bct.threshold_proportional(np.load(mat_path('sample_data.npy')), thres,
-		copy=False)
-
-def load_signed_sample(thres=1):
-	return bct.threshold_proportional(np.around(
-		np.load(mat_path('sample_signed.npy')),8), thres, copy=False)
-=======
-
 
 def load_sample(thres=1):
     return bct.threshold_proportional(np.load(mat_path('sample_data.npy')), thres,
@@ -25,7 +14,6 @@ def load_signed_sample(thres=1):
     return bct.threshold_proportional(np.around(
         np.load(mat_path('sample_signed.npy')), 8), thres, copy=False)
 
->>>>>>> 5e28b117ec6970a19a1fc978091feee2fd5c7ff6
 
 def load_sparse_sample(thres=.02):
     return load_sample(thres=thres)
@@ -36,31 +24,22 @@ def load_binary_sample(thres=.35):
 
 
 def load_directed_sample(thres=1):
-<<<<<<< HEAD
-	return bct.threshold_proportional(np.load(mat_path('sample_directed.npy')),
-		thres, copy=False)
-=======
     return bct.threshold_proportional(np.load(mat_path('sample_directed.npy')),
                                       thres, copy=False)
 
->>>>>>> 5e28b117ec6970a19a1fc978091feee2fd5c7ff6
 
 def load_binary_directed_sample(thres=.35):
     return bct.binarize(load_directed_sample(thres=thres))
 
 
 def load_directed_low_modularity_sample(thres=1):
-<<<<<<< HEAD
-	return bct.threshold_proportional(np.load(mat_path('sample_directed_gc.npy')),
-		thres, copy=False)
-=======
     return bct.threshold_proportional(np.load(mat_path('sample_directed_gc.npy')),
                                       thres, copy=False)
 
->>>>>>> 5e28b117ec6970a19a1fc978091feee2fd5c7ff6
 
 def load_binary_directed_low_modularity_sample(thres=.35):
     return bct.binarize(load_directed_low_modularity_sample(thres=thres))
+
 
 # unimplemented samples
 
@@ -90,29 +69,7 @@ def load_signed_sparse_sample(thres=.06):
 
 # NBS samples
 
-
 def load_sample_group_qball():
-<<<<<<< HEAD
-	q = np.load(mat_path('sample_group_qball.npy'))
-	return np.transpose(
-		map(bct.normalize,(q[:,:,i] for i in xrange(q.shape[2]))),
-		(1,2,0))
-
-def load_sample_group_dsi():
-	d = np.load(mat_path('sample_group_dsi.npy'))
-	return np.transpose(
-		map(bct.normalize,(d[:,:,i] for i in xrange(d.shape[2]))),
-		(1,2,0))
-
-def load_sample_group_fmri():
-	f = np.load(mat_path('sample_group_fmri.npy'))
-	import functools
-	def compose(*functions):
-		return functools.reduce(lambda f,g: lambda x: f(g(x)), functions)
-	thresh_fun = functools.partial(bct.threshold_proportional, p=.5)
-	return np.transpose(map(compose(bct.normalize,thresh_fun),
-		(f[:,:,i] for i in xrange(f.shape[2]))),(1,2,0))
-=======
     q = np.load(mat_path('sample_group_qball.npy'))
     return np.transpose(
         list(map(bct.normalize, (q[:, :, i] for i in range(q.shape[2])))),
@@ -135,4 +92,3 @@ def load_sample_group_fmri():
     thresh_fun = functools.partial(bct.threshold_proportional, p=.5)
     return np.transpose(list(map(compose(bct.normalize, thresh_fun),
                             (f[:, :, i] for i in range(f.shape[2])))), (1, 2, 0))
->>>>>>> 5e28b117ec6970a19a1fc978091feee2fd5c7ff6
