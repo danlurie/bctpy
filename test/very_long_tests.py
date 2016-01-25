17398238c96238f8ea6633d822270952b6e99ed3
aestrivex <aestrivex@gmail.com>
2016-01-25 11:08:57 -0500

    temporarily disable link_communities test

--- test/very_long_tests.py
+++ test/very_long_tests.py
@@ -3,8 +3,8 @@ import numpy as np
 import bct
 
 
-def test_link_communities():
-    x = load_sample(thres=0.4)
-    seed = 949389104
-    M = bct.link_communities(x)
-    assert np.max(M) == 1
+#def test_link_communities():
+#    x = load_sample(thres=0.4)
+#    seed = 949389104
+#    M = bct.link_communities(x)
+#    assert np.max(M) == 1
